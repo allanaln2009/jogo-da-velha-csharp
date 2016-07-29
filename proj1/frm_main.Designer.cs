ebf391641aa7df039c67aae83cd3ca3a1390e1e0
allanaln2009 <allanaln2009@gmail.com>
2016-07-29 11:25:15 -0300

    Jogo da Velha
    Jogo da Velha em C#

---
+++ proj1/frm_main.Designer.cs
@@ -0,0 +1,430 @@
+﻿namespace proj1
+{
+    partial class frm_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_main));
+			this.gb_jogadores = new System.Windows.Forms.GroupBox();
+			this.tx_jg2 = new System.Windows.Forms.TextBox();
+			this.tx_jg1 = new System.Windows.Forms.TextBox();
+			this.lb_jg1 = new System.Windows.Forms.Label();
+			this.lb_jg2 = new System.Windows.Forms.Label();
+			this.gp_placar = new System.Windows.Forms.GroupBox();
+			this.lb_placar2 = new System.Windows.Forms.Label();
+			this.lb_placar1 = new System.Windows.Forms.Label();
+			this.gb_empate = new System.Windows.Forms.GroupBox();
+			this.lb_empate = new System.Windows.Forms.Label();
+			this.gb_opcoes = new System.Windows.Forms.GroupBox();
+			this.rb_o = new System.Windows.Forms.RadioButton();
+			this.rb_x = new System.Windows.Forms.RadioButton();
+			this.bt_iniciar = new System.Windows.Forms.Button();
+			this.bt_reiniciar = new System.Windows.Forms.Button();
+			this.bt_sair = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.bt_cpu = new System.Windows.Forms.Button();
+			this.pn_btns = new System.Windows.Forms.Panel();
+			this.lb_who = new System.Windows.Forms.Label();
+			this.bt9 = new System.Windows.Forms.Button();
+			this.bt8 = new System.Windows.Forms.Button();
+			this.bt7 = new System.Windows.Forms.Button();
+			this.bt6 = new System.Windows.Forms.Button();
+			this.bt5 = new System.Windows.Forms.Button();
+			this.bt4 = new System.Windows.Forms.Button();
+			this.bt3 = new System.Windows.Forms.Button();
+			this.bt2 = new System.Windows.Forms.Button();
+			this.bt1 = new System.Windows.Forms.Button();
+			this.gb_jogadores.SuspendLayout();
+			this.gp_placar.SuspendLayout();
+			this.gb_empate.SuspendLayout();
+			this.gb_opcoes.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.pn_btns.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// gb_jogadores
+			// 
+			this.gb_jogadores.Controls.Add(this.tx_jg2);
+			this.gb_jogadores.Controls.Add(this.tx_jg1);
+			this.gb_jogadores.Controls.Add(this.lb_jg1);
+			this.gb_jogadores.Controls.Add(this.lb_jg2);
+			this.gb_jogadores.Location = new System.Drawing.Point(11, 11);
+			this.gb_jogadores.Name = "gb_jogadores";
+			this.gb_jogadores.Size = new System.Drawing.Size(182, 114);
+			this.gb_jogadores.TabIndex = 0;
+			this.gb_jogadores.TabStop = false;
+			this.gb_jogadores.Text = "Jogadores";
+			// 
+			// tx_jg2
+			// 
+			this.tx_jg2.Location = new System.Drawing.Point(36, 65);
+			this.tx_jg2.Name = "tx_jg2";
+			this.tx_jg2.Size = new System.Drawing.Size(100, 20);
+			this.tx_jg2.TabIndex = 7;
+			// 
+			// tx_jg1
+			// 
+			this.tx_jg1.Location = new System.Drawing.Point(36, 30);
+			this.tx_jg1.Name = "tx_jg1";
+			this.tx_jg1.Size = new System.Drawing.Size(100, 20);
+			this.tx_jg1.TabIndex = 6;
+			// 
+			// lb_jg1
+			// 
+			this.lb_jg1.AutoSize = true;
+			this.lb_jg1.Location = new System.Drawing.Point(6, 33);
+			this.lb_jg1.Name = "lb_jg1";
+			this.lb_jg1.Size = new System.Drawing.Size(10, 13);
+			this.lb_jg1.TabIndex = 6;
+			this.lb_jg1.Text = " ";
+			// 
+			// lb_jg2
+			// 
+			this.lb_jg2.AutoSize = true;
+			this.lb_jg2.Location = new System.Drawing.Point(6, 68);
+			this.lb_jg2.Name = "lb_jg2";
+			this.lb_jg2.Size = new System.Drawing.Size(10, 13);
+			this.lb_jg2.TabIndex = 7;
+			this.lb_jg2.Text = " ";
+			// 
+			// gp_placar
+			// 
+			this.gp_placar.Controls.Add(this.lb_placar2);
+			this.gp_placar.Controls.Add(this.lb_placar1);
+			this.gp_placar.Location = new System.Drawing.Point(199, 11);
+			this.gp_placar.Name = "gp_placar";
+			this.gp_placar.Size = new System.Drawing.Size(203, 114);
+			this.gp_placar.TabIndex = 1;
+			this.gp_placar.TabStop = false;
+			this.gp_placar.Text = "Placar";
+			// 
+			// lb_placar2
+			// 
+			this.lb_placar2.AutoSize = true;
+			this.lb_placar2.Location = new System.Drawing.Point(45, 65);
+			this.lb_placar2.Name = "lb_placar2";
+			this.lb_placar2.Size = new System.Drawing.Size(10, 13);
+			this.lb_placar2.TabIndex = 9;
+			this.lb_placar2.Text = " ";
+			// 
+			// lb_placar1
+			// 
+			this.lb_placar1.AutoSize = true;
+			this.lb_placar1.Location = new System.Drawing.Point(45, 33);
+			this.lb_placar1.Name = "lb_placar1";
+			this.lb_placar1.Size = new System.Drawing.Size(10, 13);
+			this.lb_placar1.TabIndex = 8;
+			this.lb_placar1.Text = " ";
+			// 
+			// gb_empate
+			// 
+			this.gb_empate.Controls.Add(this.lb_empate);
+			this.gb_empate.Location = new System.Drawing.Point(408, 11);
+			this.gb_empate.Name = "gb_empate";
+			this.gb_empate.Size = new System.Drawing.Size(219, 114);
+			this.gb_empate.TabIndex = 1;
+			this.gb_empate.TabStop = false;
+			this.gb_empate.Text = "Empate";
+			// 
+			// lb_empate
+			// 
+			this.lb_empate.AutoSize = true;
+			this.lb_empate.Location = new System.Drawing.Point(23, 33);
+			this.lb_empate.Name = "lb_empate";
+			this.lb_empate.Size = new System.Drawing.Size(10, 13);
+			this.lb_empate.TabIndex = 9;
+			this.lb_empate.Text = " ";
+			// 
+			// gb_opcoes
+			// 
+			this.gb_opcoes.Controls.Add(this.rb_o);
+			this.gb_opcoes.Controls.Add(this.rb_x);
+			this.gb_opcoes.Location = new System.Drawing.Point(11, 131);
+			this.gb_opcoes.Name = "gb_opcoes";
+			this.gb_opcoes.Size = new System.Drawing.Size(182, 74);
+			this.gb_opcoes.TabIndex = 2;
+			this.gb_opcoes.TabStop = false;
+			this.gb_opcoes.Text = "Opções";
+			// 
+			// rb_o
+			// 
+			this.rb_o.AutoSize = true;
+			this.rb_o.Location = new System.Drawing.Point(103, 35);
+			this.rb_o.Name = "rb_o";
+			this.rb_o.Size = new System.Drawing.Size(33, 17);
+			this.rb_o.TabIndex = 11;
+			this.rb_o.Text = "O";
+			this.rb_o.UseVisualStyleBackColor = true;
+			// 
+			// rb_x
+			// 
+			this.rb_x.AutoSize = true;
+			this.rb_x.Checked = true;
+			this.rb_x.Location = new System.Drawing.Point(24, 35);
+			this.rb_x.Name = "rb_x";
+			this.rb_x.Size = new System.Drawing.Size(32, 17);
+			this.rb_x.TabIndex = 10;
+			this.rb_x.TabStop = true;
+			this.rb_x.Text = "X";
+			this.rb_x.UseVisualStyleBackColor = true;
+			// 
+			// bt_iniciar
+			// 
+			this.bt_iniciar.Location = new System.Drawing.Point(301, 182);
+			this.bt_iniciar.Name = "bt_iniciar";
+			this.bt_iniciar.Size = new System.Drawing.Size(75, 23);
+			this.bt_iniciar.TabIndex = 3;
+			this.bt_iniciar.Text = "Iniciar";
+			this.bt_iniciar.UseVisualStyleBackColor = true;
+			this.bt_iniciar.Click += new System.EventHandler(this.bt_iniciar_Click);
+			// 
+			// bt_reiniciar
+			// 
+			this.bt_reiniciar.Location = new System.Drawing.Point(425, 182);
+			this.bt_reiniciar.Name = "bt_reiniciar";
+			this.bt_reiniciar.Size = new System.Drawing.Size(75, 23);
+			this.bt_reiniciar.TabIndex = 4;
+			this.bt_reiniciar.Text = "Reiniciar";
+			this.bt_reiniciar.UseVisualStyleBackColor = true;
+			this.bt_reiniciar.Click += new System.EventHandler(this.bt_reiniciar_Click);
+			// 
+			// bt_sair
+			// 
+			this.bt_sair.Location = new System.Drawing.Point(542, 182);
+			this.bt_sair.Name = "bt_sair";
+			this.bt_sair.Size = new System.Drawing.Size(75, 23);
+			this.bt_sair.TabIndex = 5;
+			this.bt_sair.Text = "Sair";
+			this.bt_sair.UseVisualStyleBackColor = true;
+			this.bt_sair.Click += new System.EventHandler(this.bt_sair_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.bt_cpu);
+			this.panel1.Controls.Add(this.gb_jogadores);
+			this.panel1.Controls.Add(this.bt_sair);
+			this.panel1.Controls.Add(this.gb_empate);
+			this.panel1.Controls.Add(this.bt_reiniciar);
+			this.panel1.Controls.Add(this.bt_iniciar);
+			this.panel1.Controls.Add(this.gp_placar);
+			this.panel1.Controls.Add(this.gb_opcoes);
+			this.panel1.Location = new System.Drawing.Point(6, 7);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(645, 215);
+			this.panel1.TabIndex = 6;
+			// 
+			// bt_cpu
+			// 
+			this.bt_cpu.Location = new System.Drawing.Point(204, 182);
+			this.bt_cpu.Name = "bt_cpu";
+			this.bt_cpu.Size = new System.Drawing.Size(75, 23);
+			this.bt_cpu.TabIndex = 6;
+			this.bt_cpu.Text = "Contra CPU";
+			this.bt_cpu.UseVisualStyleBackColor = true;
+			this.bt_cpu.Click += new System.EventHandler(this.bt_cpu_Click);
+			// 
+			// pn_btns
+			// 
+			this.pn_btns.Controls.Add(this.lb_who);
+			this.pn_btns.Controls.Add(this.bt9);
+			this.pn_btns.Controls.Add(this.bt8);
+			this.pn_btns.Controls.Add(this.bt7);
+			this.pn_btns.Controls.Add(this.bt6);
+			this.pn_btns.Controls.Add(this.bt5);
+			this.pn_btns.Controls.Add(this.bt4);
+			this.pn_btns.Controls.Add(this.bt3);
+			this.pn_btns.Controls.Add(this.bt2);
+			this.pn_btns.Controls.Add(this.bt1);
+			this.pn_btns.Enabled = false;
+			this.pn_btns.Location = new System.Drawing.Point(6, 228);
+			this.pn_btns.Name = "pn_btns";
+			this.pn_btns.Size = new System.Drawing.Size(645, 238);
+			this.pn_btns.TabIndex = 6;
+			// 
+			// lb_who
+			// 
+			this.lb_who.AutoSize = true;
+			this.lb_who.Location = new System.Drawing.Point(17, 26);
+			this.lb_who.Name = "lb_who";
+			this.lb_who.Size = new System.Drawing.Size(0, 13);
+			this.lb_who.TabIndex = 9;
+			// 
+			// bt9
+			// 
+			this.bt9.Location = new System.Drawing.Point(353, 148);
+			this.bt9.Name = "bt9";
+			this.bt9.Size = new System.Drawing.Size(62, 55);
+			this.bt9.TabIndex = 8;
+			this.bt9.Text = "X";
+			this.bt9.UseVisualStyleBackColor = true;
+			this.bt9.Click += new System.EventHandler(this.bt9_Click);
+			// 
+			// bt8
+			// 
+			this.bt8.Location = new System.Drawing.Point(285, 148);
+			this.bt8.Name = "bt8";
+			this.bt8.Size = new System.Drawing.Size(62, 55);
+			this.bt8.TabIndex = 7;
+			this.bt8.Text = "X";
+			this.bt8.UseVisualStyleBackColor = true;
+			this.bt8.Click += new System.EventHandler(this.bt8_Click);
+			// 
+			// bt7
+			// 
+			this.bt7.Location = new System.Drawing.Point(217, 148);
+			this.bt7.Name = "bt7";
+			this.bt7.Size = new System.Drawing.Size(62, 55);
+			this.bt7.TabIndex = 6;
+			this.bt7.Text = "X";
+			this.bt7.UseVisualStyleBackColor = true;
+			this.bt7.Click += new System.EventHandler(this.bt7_Click);
+			// 
+			// bt6
+			// 
+			this.bt6.Location = new System.Drawing.Point(353, 87);
+			this.bt6.Name = "bt6";
+			this.bt6.Size = new System.Drawing.Size(62, 55);
+			this.bt6.TabIndex = 5;
+			this.bt6.Text = "X";
+			this.bt6.UseVisualStyleBackColor = true;
+			this.bt6.Click += new System.EventHandler(this.bt6_Click);
+			// 
+			// bt5
+			// 
+			this.bt5.Location = new System.Drawing.Point(285, 87);
+			this.bt5.Name = "bt5";
+			this.bt5.Size = new System.Drawing.Size(62, 55);
+			this.bt5.TabIndex = 4;
+			this.bt5.Text = "X";
+			this.bt5.UseVisualStyleBackColor = true;
+			this.bt5.Click += new System.EventHandler(this.bt5_Click);
+			// 
+			// bt4
+			// 
+			this.bt4.Location = new System.Drawing.Point(217, 87);
+			this.bt4.Name = "bt4";
+			this.bt4.Size = new System.Drawing.Size(62, 55);
+			this.bt4.TabIndex = 3;
+			this.bt4.Text = "X";
+			this.bt4.UseVisualStyleBackColor = true;
+			this.bt4.Click += new System.EventHandler(this.bt4_Click);
+			// 
+			// bt3
+			// 
+			this.bt3.Location = new System.Drawing.Point(353, 26);
+			this.bt3.Name = "bt3";
+			this.bt3.Size = new System.Drawing.Size(62, 55);
+			this.bt3.TabIndex = 2;
+			this.bt3.Text = "X";
+			this.bt3.UseVisualStyleBackColor = true;
+			this.bt3.Click += new System.EventHandler(this.bt3_Click);
+			// 
+			// bt2
+			// 
+			this.bt2.Location = new System.Drawing.Point(285, 26);
+			this.bt2.Name = "bt2";
+			this.bt2.Size = new System.Drawing.Size(62, 55);
+			this.bt2.TabIndex = 1;
+			this.bt2.Text = "X";
+			this.bt2.UseVisualStyleBackColor = true;
+			this.bt2.Click += new System.EventHandler(this.bt2_Click);
+			// 
+			// bt1
+			// 
+			this.bt1.Location = new System.Drawing.Point(217, 26);
+			this.bt1.Name = "bt1";
+			this.bt1.Size = new System.Drawing.Size(62, 55);
+			this.bt1.TabIndex = 0;
+			this.bt1.Text = "X";
+			this.bt1.UseVisualStyleBackColor = true;
+			this.bt1.Click += new System.EventHandler(this.bt1_Click);
+			// 
+			// frm_main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(656, 483);
+			this.Controls.Add(this.pn_btns);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "frm_main";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Jogo da Velha";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frm_main_FormClosing);
+			this.gb_jogadores.ResumeLayout(false);
+			this.gb_jogadores.PerformLayout();
+			this.gp_placar.ResumeLayout(false);
+			this.gp_placar.PerformLayout();
+			this.gb_empate.ResumeLayout(false);
+			this.gb_empate.PerformLayout();
+			this.gb_opcoes.ResumeLayout(false);
+			this.gb_opcoes.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.pn_btns.ResumeLayout(false);
+			this.pn_btns.PerformLayout();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb_jogadores;
+        private System.Windows.Forms.GroupBox gp_placar;
+        private System.Windows.Forms.GroupBox gb_empate;
+        private System.Windows.Forms.GroupBox gb_opcoes;
+        private System.Windows.Forms.TextBox tx_jg2;
+        private System.Windows.Forms.TextBox tx_jg1;
+        private System.Windows.Forms.Label lb_jg1;
+        private System.Windows.Forms.Label lb_jg2;
+        private System.Windows.Forms.Label lb_placar1;
+        private System.Windows.Forms.Label lb_empate;
+        private System.Windows.Forms.RadioButton rb_o;
+        private System.Windows.Forms.RadioButton rb_x;
+        private System.Windows.Forms.Button bt_iniciar;
+        private System.Windows.Forms.Button bt_reiniciar;
+        private System.Windows.Forms.Button bt_sair;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pn_btns;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt1;
+		private System.Windows.Forms.Label lb_placar2;
+		private System.Windows.Forms.Button bt_cpu;
+		private System.Windows.Forms.Label lb_who;
+
+    }
+}
+
---
+++ proj1/frm_main.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proj1 {
+	public partial class frm_main : Form {
+		string nomeJg1, nomeJg2, jogada, letraJg1, letraJg2, nX = "", nO = "";
+		int cont = 0, pontoJg1 = 0, pontoJg2 = 0, empates = 0, cpujoga = 0, estiloJogo = 0;
+
+		public frm_main() {
+			InitializeComponent();
+			limpar(0); // 0 pra limpar tudo
+			tx_jg1.Focus();
+		}
+
+
+
+		private void frm_main_FormClosing(object sender, FormClosingEventArgs e) {
+			if (MessageBox.Show("Deseja fechar o Jogo da Velha?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2) == DialogResult.No) {
+				e.Cancel = true;
+			}
+		}
+
+		private void bt_sair_Click(object sender, EventArgs e) {
+			Close();
+		}
+
+		public void jogada_artificial() {
+			if (cpujoga == 1) {
+				if (bt1.Enabled == true) {
+					Button bt = (Button)bt1;
+					trocaLetra(bt);
+					return;
+				}
+
+				if (bt2.Enabled == false && bt7.Enabled == true) {
+					Button bt = (Button)bt7;
+					trocaLetra(bt);
+					estiloJogo = 1;
+					return;
+				} else if (bt3.Enabled == false && bt7.Enabled == true) {
+					Button bt = (Button)bt7;
+					trocaLetra(bt);
+					estiloJogo = 2;
+					return;
+				} else if (bt3.Enabled == false && bt9.Enabled == true) {
+					Button bt = (Button)bt9;
+					trocaLetra(bt);
+					estiloJogo = 3;
+					return;
+				} else if (bt4.Enabled == false && bt3.Enabled == true) {
+					Button bt = (Button)bt3;
+					trocaLetra(bt);
+					estiloJogo = 4;
+					return;
+				} else if (bt5.Enabled == false && bt9.Enabled == true) {
+					Button bt = (Button)bt9;
+					trocaLetra(bt);
+					estiloJogo = 5;
+					return;
+				} else if (bt6.Enabled == false && bt3.Enabled == true) {
+					Button bt = (Button)bt3;
+					trocaLetra(bt);
+					estiloJogo = 6;
+					return;
+				} else if (bt7.Enabled == false && bt3.Enabled == true) {
+					Button bt = (Button)bt3;
+					trocaLetra(bt);
+					estiloJogo = 7;
+					return;
+				} else if (bt8.Enabled == false && bt3.Enabled == true) {
+					Button bt = (Button)bt3;
+					trocaLetra(bt);
+					estiloJogo = 8;
+					return;
+				} else if (bt8.Enabled == false && bt7.Enabled == true) {
+					// A e B
+					Button bt = (Button)bt7;
+					trocaLetra(bt);
+					estiloJogo = 9;
+					return;
+				} else if (bt9.Enabled == false && bt3.Enabled == true) {
+					Button bt = (Button)bt3;
+					trocaLetra(bt);
+					estiloJogo = 10;
+					return;
+				}
+			}
+		}
+
+		public void trocaLetra(Button bt) {
+			bt.Text = jogada;
+			if (jogada == "X") {
+				jogada = "O";
+				rb_o.Checked = true;
+				//lb_who.Text = "Vez de " + nO;
+			} else {
+				jogada = "X";
+				rb_x.Checked = true;
+				//lb_who.Text = "Vez de " + nX;
+			}
+			wholabel_change();
+			bt.Enabled = false;
+			cont++;
+			gb_jogadores.Focus();
+			vencedor();
+		}
+
+		public void vencedor() {
+			if (cont >= 5) {
+				/// Verifica se O ganhou
+				if (
+					// Linhas Horizontais
+					(bt1.Text == "O") && (bt2.Text == "O") && (bt3.Text == "O") ||
+					(bt4.Text == "O") && (bt5.Text == "O") && (bt6.Text == "O") ||
+					(bt7.Text == "O") && (bt8.Text == "O") && (bt9.Text == "O") ||
+					// Linhas Verticas
+					(bt1.Text == "O") && (bt4.Text == "O") && (bt7.Text == "O") ||
+					(bt2.Text == "O") && (bt5.Text == "O") && (bt8.Text == "O") ||
+					(bt3.Text == "O") && (bt6.Text == "O") && (bt9.Text == "O") ||
+					// Diagonais
+					(bt1.Text == "O") && (bt5.Text == "O") && (bt9.Text == "O") ||
+					(bt3.Text == "O") && (bt5.Text == "O") && (bt7.Text == "O")
+				   ) {
+					if (letraJg1 == "O") {
+						MessageBox.Show(nomeJg1 + " Venceu!", "Parabéns", MessageBoxButtons.OK);
+						pontoJg1++;
+						rb_o.Checked = true;
+						jogada = "O";
+					} else if (letraJg2 == "O") {
+						MessageBox.Show(nomeJg2 + " Venceu!", "Parabéns", MessageBoxButtons.OK);
+						pontoJg2++;
+						rb_o.Checked = true;
+						jogada = "O";
+					}
+					atualiza_placar();
+					limpar(1);	//1 pra limpar só botões e contador
+					habilita_btns(1);
+				}
+				/// Verifica se X ganhou
+				if (
+					// Linhas Horizontais
+					(bt1.Text == "X") && (bt2.Text == "X") && (bt3.Text == "X") ||
+					(bt4.Text == "X") && (bt5.Text == "X") && (bt6.Text == "X") ||
+					(bt7.Text == "X") && (bt8.Text == "X") && (bt9.Text == "X") ||
+					// Linhas Verticas
+					(bt1.Text == "X") && (bt4.Text == "X") && (bt7.Text == "X") ||
+					(bt2.Text == "X") && (bt5.Text == "X") && (bt8.Text == "X") ||
+					(bt3.Text == "X") && (bt6.Text == "X") && (bt9.Text == "X") ||
+					// Diagonais
+					(bt1.Text == "X") && (bt5.Text == "X") && (bt9.Text == "X") ||
+					(bt3.Text == "X") && (bt5.Text == "X") && (bt7.Text == "X")
+				   ) {
+					if (letraJg1 == "X") {
+						MessageBox.Show(nomeJg1 + " Venceu!", "Parabéns", MessageBoxButtons.OK);
+						pontoJg1++;
+						rb_x.Checked = true;
+						jogada = "X";
+					} else if (letraJg2 == "X") {
+						MessageBox.Show(nomeJg2 + " Venceu!", "Parabéns", MessageBoxButtons.OK);
+						pontoJg2++;
+						rb_x.Checked = true;
+						jogada = "X";
+					}
+					atualiza_placar();
+					limpar(1);	//1 pra limpar só botões e contador
+					habilita_btns(1);
+				}
+				/// Verifica se empatou
+				if(cont == 9){
+					MessageBox.Show("Empate!", "Poxa, deu velha :(", MessageBoxButtons.OK);
+					empates++;
+					atualiza_placar();
+					limpar(1);	//1 pra limpar só botões e contador
+					habilita_btns(1);
+				}
+			}
+		}
+
+		public void limpar(int identificador) {
+			bt1.Text = "";
+			bt2.Text = "";
+			bt3.Text = "";
+			bt4.Text = "";
+			bt5.Text = "";
+			bt6.Text = "";
+			bt7.Text = "";
+			bt8.Text = "";
+			bt9.Text = "";
+			lb_who.Text = "";
+			cont = 0;
+
+			if (identificador == 0) {
+				tx_jg1.Text = "";
+				tx_jg2.Text = "";
+				lb_jg1.Text = "";
+				lb_jg2.Text = "";
+				lb_empate.Text = "";
+				lb_placar1.Text = "";
+				lb_placar2.Text = "";
+				lb_who.Text = "";
+
+				letraJg1 = "";
+				letraJg2 = "";
+
+				pontoJg1 = 0;
+				pontoJg2 = 0;
+				empates = 0;
+			}
+		}
+
+		public void habilita_btns(int identificador) {
+			bt1.Enabled = true;
+			bt2.Enabled = true;
+			bt3.Enabled = true;
+			bt4.Enabled = true;
+			bt5.Enabled = true;
+			bt6.Enabled = true;
+			bt7.Enabled = true;
+			bt8.Enabled = true;
+			bt9.Enabled = true;
+
+			if (identificador == 0) {
+				bt_iniciar.Enabled = true;
+				bt_cpu.Enabled = true;
+				tx_jg1.Visible = true;
+				tx_jg2.Visible = true;
+				gb_opcoes.Enabled = true;
+				pn_btns.Enabled = false;
+				rb_x.Checked = true;
+
+				lb_who.Text = "";
+				tx_jg2.Text = "";
+				tx_jg2.Visible = true;
+				bt_cpu.Text = "Contra CPU";
+				cpujoga = 0;
+			}
+			wholabel_change();
+		}
+
+		public void atualiza_placar() {
+			string t1, t2, t3;
+			if(pontoJg1 > 1){
+				t1	= " Vitórias";
+			}else{
+				t1	= " Vitória";
+			}
+			if (pontoJg2 > 1) {
+				t2 = " Vitórias";
+			} else {
+				t2 = " Vitória";
+			}
+			if (empates > 1) {
+				t3 = " Empates";
+			} else {
+				t3 = " Empate";
+			}
+
+			lb_placar1.Text = pontoJg1.ToString() + t1;
+			lb_placar2.Text = pontoJg2.ToString() + t2;
+			lb_empate.Text = empates.ToString() + t3;
+
+			wholabel_change();
+		}
+
+		public void wholabel_change() {
+			if (cont < 8) {	
+				if (letraJg1 == "X") {
+					nX = nomeJg1;
+					nO = nomeJg2;
+				} else {
+					nX = nomeJg2;
+					nO = nomeJg1;
+				}
+				if (rb_x.Checked == true) {
+					lb_who.Text = "Vez de " + nX;
+				} else {
+					lb_who.Text = "Vez de " + nO;
+				}
+			}
+		}
+
+		private void bt_iniciar_Click(object sender, EventArgs e) {
+			nomeJg1 = tx_jg1.Text;
+			nomeJg2 = tx_jg2.Text;
+			if (nomeJg1 == "" || nomeJg2 == "") {
+				MessageBox.Show("Digite os nomes dos jogadores.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+				if (nomeJg1 == "") {
+					tx_jg1.Focus();
+				} else {
+					tx_jg2.Focus();
+				}
+				//Application.ExitThread();
+				return;
+			}
+
+			tx_jg1.Visible = false;
+			lb_jg1.Text = nomeJg1 + " - Joga com ";
+
+			tx_jg2.Visible = false;
+			lb_jg2.Text = nomeJg2 + " - Joga com ";
+
+			lb_who.Visible = true;
+
+			if (rb_x.Checked) {
+				lb_jg1.Text = lb_jg1.Text + "X";
+				lb_jg2.Text = lb_jg2.Text + "O";
+				letraJg1 = "X";
+				letraJg2 = "O";
+			} else {
+				lb_jg1.Text = lb_jg1.Text + "O";
+				lb_jg2.Text = lb_jg2.Text + "X";
+				letraJg1 = "O";
+				letraJg2 = "X";
+			}
+
+			atualiza_placar();
+			//wholabel_change();
+			jogada = "X";
+			rb_x.Checked = true;
+			gb_opcoes.Enabled = false;
+			bt_iniciar.Enabled = false;
+			bt_cpu.Enabled = false;
+			pn_btns.Enabled = true;
+		}
+
+		private void bt_reiniciar_Click(object sender, EventArgs e) {
+			limpar(0);	// 0 pra limpar tudo
+			habilita_btns(0);
+			lb_who.Visible = false;
+		}
+
+		private void bt_cpu_Click(object sender, EventArgs e) {
+			if (cpujoga == 0) {
+				tx_jg2.Text = "CPU";
+				tx_jg2.Visible = false;
+				bt_cpu.Text = "Contra 2";
+				cpujoga++;
+			} else if (cpujoga == 1) {
+				tx_jg2.Text = "";
+				tx_jg2.Visible = true;
+				bt_cpu.Text = "Contra CPU";
+				cpujoga = 0;
+			}
+		}
+
+		private void bt1_Click(object sender, EventArgs e) {
+			//bt1.Text = jogada;
+			Button bt = (Button)sender;
+			trocaLetra(bt);
+			jogada_artificial();
+		}
+
+		private void bt2_Click(object sender, EventArgs e) {
+			//bt2.Text = jogada;
+			Button bt = (Button)sender;
+			trocaLetra(bt);
+			jogada_artificial();
+		}
+
+		private void bt3_Click(object sender, EventArgs e) {
+			//bt3.Text = jogada;
+			Button bt = (Button)sender;
+			trocaLetra(bt);
+			jogada_artificial();
+		}
+
+		private void bt4_Click(object sender, EventArgs e) {
+			//bt4.Text = jogada;
+			Button bt = (Button)sender;
+			trocaLetra(bt);
+			jogada_artificial();
+		}
+
+		private void bt5_Click(object sender, EventArgs e) {
+			//bt5.Text = jogada;
+			Button bt = (Button)sender;
+			trocaLetra(bt);
+			jogada_artificial();
+		}
+
+		private void bt6_Click(object sender, EventArgs e) {
+			//bt6.Text = jogada;
+			Button bt = (Button)sender;
+			trocaLetra(bt);
+			jogada_artificial();
+		}
+
+		private void bt7_Click(object sender, EventArgs e) {
+			//bt7.Text = jogada;
+			Button bt = (Button)sender;
+			trocaLetra(bt);
+			jogada_artificial();
+		}
+
+		private void bt8_Click(object sender, EventArgs e) {
+			//bt8.Text = jogada;
+			Button bt = (Button)sender;
+			trocaLetra(bt);
+			jogada_artificial();
+		}
+
+		private void bt9_Click(object sender, EventArgs e) {
+			//bt9.Text = jogada;
+			Button bt = (Button)sender;
+			trocaLetra(bt);
+			jogada_artificial();
+		}
+
+	}
+}
